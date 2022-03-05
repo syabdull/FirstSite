1a175d6455bd5a0bbd81bb2613eaaa05395b0b2e
syabdull <syab5050@gmail.com>
2022-03-05 18:41:37 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+function animatedForm(){
+    const arrows=document.querySelectorAll(".fa-arrow-down");
+    arrows.forEach(arrow=> {
+        arrow.addEventListener("click", () => {
+            const input=arrow.previousElementSibling;
+            const parent=arrow.parentElement;
+            const nextForm =parent.nextElementSibling;
+            //check for validation
+            if(input.type === "text" && validateUser(input)){
+                console.log("everything is okay!");
+
+            }
+        });
+    });
+
+}
+function validateUser(user){
+    if(user.value.length<6){
+        console.log("not enough characters");
+        error("rgb(189,87,87");
+    }else{
+        error("rgb(187,189,130)");
+        return true;
+
+    }
+
+    }
+
+
+function error(color){
+    document.body.style.backgroundColor=color;
+
+}
+ animatedForm();
